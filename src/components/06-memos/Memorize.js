b67480f656268258cc6f81045a7527bbd57e044f
Santiago Pegels <santiagopegels@gmail.com>
2021-06-03 10:11:03 -0300

    React.memo() example

---
+++ src/components/06-memos/Memorize.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { useCounter } from '../../hooks/useCounter'
+import { Small } from './Small'
+
+export const Memorize = () => {
+
+    const { state: counter, increment } = useCounter(10)
+    const [show, setShow] = useState(true)
+
+    return (
+        <div>
+            <h1>Counter: <Small value={counter} /></h1>
+            <hr></hr>
+
+            <button
+                className="btn btn-primary"
+                onClick={increment}
+            >
+                +1
+            </button>
+
+            <button
+                className="btn btn-outline-primary ms-3"
+                onClick={() => {
+                    setShow(!show)
+                }}
+            >
+                Show/Hide {JSON.stringify(show)}
+            </button>
+        </div>
+    )
+}
---
+++ src/components/06-memos/Small.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+/* El Memo se utliza para que este componente solo se actualice si cambia la prop value en este caso. Se utiliza
+mucho cuando hay llamadas fetch para evitar que vuelva a hacerlas en caso de que cambie el state del componente padre 
+por ejemplo, que no afecta en nada a este. */
+
+export const Small = React.memo(({value}) => {
+    return (
+        <small>
+            {value}
+        </small>
+    )
+})
