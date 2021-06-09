94660125e3aaa50e26c575017203abe0331af762
Santiago Pegels <santiagopegels@gmail.com>
2021-06-09 09:02:54 -0300

    useCounter complete tests

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,5 +1,5 @@
 import {useCounter} from '../../hooks/useCounter'
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 
 describe('hook useCounter test', () => {
     test('should return default values', () => {
@@ -10,8 +10,59 @@ describe('hook useCounter test', () => {
         expect(typeof result.current.increment).toBe('function')
         expect(typeof result.current.decrement).toBe('function')
         expect(typeof result.current.reset).toBe('function')
+    })
 
+    test('should useCounter return value 100', () => {
+                
+        const {result} = renderHook( ()=> useCounter(100) )
 
+        expect(result.current.state).toBe(100)
+    })
+    
+    test('should useCounter increment the state', () => {
+                
+        const {result} = renderHook( ()=> useCounter(100) )
+
+        const {increment} = result.current
+        
+        //permite ejecutar funciones de los hooks
+        act( () => {
+            increment()
+        })
+
+        const { state } = result.current
+        expect(state).toBe(101)
+    })
+
+    test('should useCounter decrement the state', () => {
+                
+        const {result} = renderHook( ()=> useCounter(100) )
+
+        const {decrement} = result.current
+        
+        //permite ejecutar funciones de los hooks
+        act( () => {
+            decrement()
+        })
+
+        const { state } = result.current
+        expect(state).toBe(99)
+    })
+
+    test('should useCounter reset the state', () => {
+                
+        const {result} = renderHook( ()=> useCounter(100) )
+
+        const {decrement, reset} = result.current
+        
+        //permite ejecutar funciones de los hooks
+        act( () => {
+            decrement()
+            reset()
+        })
+
+        const { state } = result.current
+        expect(state).toBe(100)
     })
     
 })
